d7a7944f1cea40a05642b5d50731ff8119b9253d
Hung Guei <hguei@moporgic.info>
2022-09-19 14:45:17 +0800

    update comments

--- 2048.cpp
+++ 2048.cpp
@@ -4,15 +4,15 @@
  * https://github.com/moporgic/TDL2048-Demo
  *
  * Computer Games and Intelligence (CGI) Lab, NCTU, Taiwan
- * http://www.aigames.nctu.edu.tw
+ * https://cgilab.nctu.edu.tw
  *
  * References:
- * [1] Szubert, Marcin, and Wojciech Jaśkowski. "Temporal difference learning of n-tuple networks for the game 2048."
- * Computational Intelligence and Games (CIG), 2014 IEEE Conference on. IEEE, 2014.
+ * [1] Szubert, Marcin and Wojciech Jaśkowski. "Temporal difference learning of n-tuple networks for the game 2048."
+ *     Computational Intelligence and Games (CIG), 2014 IEEE Conference on. IEEE, 2014.
  * [2] Wu, I-Chen, et al. "Multi-stage temporal difference learning for 2048."
- * Technologies and Applications of Artificial Intelligence. Springer International Publishing, 2014. 366-378.
- * [3] Oka, Kazuto, and Kiminori Matsuzaki. "Systematic selection of n-tuple networks for 2048."
- * International Conference on Computers and Games. Springer International Publishing, 2016.
+ *     Technologies and Applications of Artificial Intelligence. Springer International Publishing, 2014. 366-378.
+ * [3] Oka, Kazuto and Kiminori Matsuzaki. "Systematic selection of n-tuple networks for 2048."
+ *     International Conference on Computers and Games. Springer International Publishing, 2016.
  */
 #include <iostream>
 #include <algorithm>
